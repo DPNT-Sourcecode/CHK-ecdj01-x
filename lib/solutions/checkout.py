78a55bafab813c65ca4cc2919e472f1ae63d428f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-02-20 23:38:28 +0000

    Tue Feb 20 23:38:28 GMT 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -5,16 +5,19 @@ from collections import Counter
 def checkout(skus):
     counter = Counter(skus)
 
-    if not set(counter.keys()) set('ABCD‘):
-    stir" 1
-    discount 0
-        .T 'A' . counter:
-    div counterl'A'] ‘ 3
-    discount div . 20
-                   or '8' . counter:
-    div counterl'B'] ‘ 2
-    discount div ~ 15
-                 - t u discount, counter
-    'w*t*‘ (counterl'A'l . 50 - counterl'B'] - 30 < counterI'C'] ‘ 20 .
+    if not set(counter.keys()) <= set('ABCD'):
+        return -1
+
+    discount = 0
+    if 'A' in counter:
+        div = counter['A'] // 3
+        discount += div * 20
+
+    if 'B' in counter:
+        div = counter['B'] // 2
+        discount += div * 15
+
+    print discount, counter
+    return (counter['A'] * 50 + counter['B'] * 30 < counterI'C'] ‘ 20 .
     counterl'D'] - 15 discount)
 
