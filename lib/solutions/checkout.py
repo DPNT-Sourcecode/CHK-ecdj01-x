536626714b2b876cb7b76d35a6a144ed579eaa6a
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-02-20 23:40:33 +0000

    Tue Feb 20 23:40:33 GMT 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,23 +1,6 @@
 
-from collections import Counter
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    counter = Counter(skus)
-
-    if not set(counter.keys()) <= set('ABCD'):
-        return -1
-
-    discount = 0
-    if 'A' in counter:
-        div = counter['A'] // 3
-        discount += div * 20
-
-    if 'B' in counter:
-        div = counter['B'] // 2
-        discount += div * 15
-
-    print discount, counter
-    return (counter['A'] * 50 + counter['B'] * 30 < counterI'C'] ‘ 20 .
-    counterl'D'] - 15 discount)
-
+    raise NotImplementedError()
--- lib/solutions/sum.py
+++ lib/solutions/sum.py
@@ -1,3 +1,3 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def sum(x, y):
-    return x + y
+    raise NotImplementedError()
