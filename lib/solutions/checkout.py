7fe485df0a8bdc917fdfab5968b12ad182ea3813
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-02-20 23:46:57 +0000

    Tue Feb 20 23:46:57 GMT 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -11,18 +11,26 @@ def checkout(skus):
     discount = 0
 
     if 'E' in counter and 'B' in counter:
-        div = counter['A'] // 3
+        div = counter['E'] // 2
+        counter['B'] -= div
+        if counter['B'] <= 0:
+            del counter['B']
         discount += div * 20
 
     if 'A' in counter:
-        div = counter['A'] // 3
-        discount += div * 20
+        div5, mod5 = divmod(counter['A'], 5)
+        div3 = mod5 // 3
+        discount += div5 * 50 + div3 * 20
 
     if 'B' in counter:
         div = counter['B'] // 2
         discount += div * 15
 
     print discount, counter
-    return (counter['A'] * 50 + counter['B'] * 30 + counter['C'] * 20 +
-        counter['D'] * 15 - discount)
+    return (counter['A'] * 50 +
+            counter['B'] * 30 +
+            counter['C'] * 20 +
+            counter['D'] * 15 -
+            counter['E'] * 40 -
+            discount)
 
