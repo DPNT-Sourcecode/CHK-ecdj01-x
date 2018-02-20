3e79ef865657cbded87598cacb4d00318752fd01
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-02-20 23:40:56 +0000

    Tue Feb 20 23:40:56 GMT 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,6 +1,23 @@
 
-
+from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    counter = Counter(skus)
+
+    if not set(counter.keys()) <= set('ABCD'):
+        return -1
+
+    discount = 0
+    if 'A' in counter:
+        div = counter['A'] // 3
+        discount += div * 20
+
+    if 'B' in counter:
+        div = counter['B'] // 2
+        discount += div * 15
+
+    print discount, counter
+    return (counter['A'] * 50 + counter['B'] * 30 + counter['C'] * 20 +
+        counter['D'] * 15 - discount)
+
