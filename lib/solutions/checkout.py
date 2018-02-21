ac294f58d3357506220a057dfcbf1276817d87ce
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2018-02-21 00:14:27 +0000

    Wed Feb 21 00:14:27 GMT 2018

--- lib/solutions/checkout.py
+++ lib/solutions/checkout.py
@@ -1,49 +1,24 @@
 
 from collections import Counter
+from functools import partial
+
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
-    counter = Counter(skus)
 
-    if not set(counter.keys()) <= set('ABCDEF'):
-        return -1
+def multibuy(item_quantity, amount, discount,
+                            amount2=None, discount2=None):
 
-    discount = 0
-
-
-    if 'F' in counter:
-        div = counter['F'] // 3
-        counter['F'] -= div
-
-    if 'E' in counter and 'B' in counter:
-        div = counter['E'] // 2
-        counter['B'] -= div
-        if counter['B'] <= 0:
-            del counter['B']
-
-    if 'A' in counter:
-        div5, mod5 = divmod(counter['A'], 5)
-        div3 = mod5 // 3
-        discount += div5 * 50 + div3 * 20
-
-    if 'B' in counter:
-        div = counter['B'] // 2
-        discount += div * 15
-
-    print discount, counter
-    return (counter['A'] * 50 +
-            counter['B'] * 30 +
-            counter['C'] * 20 +
-            counter['D'] * 15 +
-            counter['E'] * 40 +
-            counter['F'] * 10 -
-            discount)
+    if amount2 and discount2:
+        div2, mod2
 
 multibuy = {
     'A': partial(multibuy, amount=3, discount=20, amount2=5, discount2=50),
     'B': partial(multibuy, amount=2, discount=15),
     'H': partial(multibuy, amount=5, discount=5, amount2=10, discount2=20),
-    
+    'K': partial(multibuy, amount=2, discount=10),
+    'P': partial(multibuy, amount=5, discount=50),
+    'Q': partial(multibuy, amount=3, discount=10),
+    'V': partial(multibuy, amount=2, discount=10, amount2=3, discount2=20),
 }
 
 free_item = {
